a2d89c859b40705c14bfd2949359682798c5c1f8
Cathy Diaz <Cathydiaz14@gmail.com>
2021-03-23 20:48:25 -0700

    commit tree a2cae48cfe024124b287a656185f1c867333793a

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
+require './config/environment'
 class UsersController < ApplicationController
 
 
+    get '/users/:slug' do
+        @tweets=Tweet.all.select{|x|x.user_id == params[:id].to_i}
+         erb :'users/show'
+     end
+
+
+     
 end
